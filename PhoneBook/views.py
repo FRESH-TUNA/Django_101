a3b2e03d6124f3bbcb2671ce198fccc20072071f
DONGWON KIM <lunacircle4@gmail.com>
2019-05-01 00:32:28 +0900

    index template 작성

--- PhoneBook/views.py
+++ PhoneBook/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 # render한결과를 반드시 return 하자
 def index(request):
-    return render(request, "PhoneBook/index.html")
+    phoneNumbers = Phonebook.objects.all()
+    return render(request, "PhoneBook/index.html", {'phoneNumbers':phoneNumbers})
+
+def about(request):
+    return render(request, "PhoneBook/about.html")
