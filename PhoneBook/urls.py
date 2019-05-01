54b05a1fc152cf260ded87caff1ddbca9cb690eb
DONGWON KIM <lunacircle4@gmail.com>
2019-05-01 18:13:24 +0900

    index template 작성완료

--- PhoneBook/urls.py
+++ PhoneBook/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('index', views.index, name="index"),
-    path('about', views.about, name="about")
+    path('create', views.create, name="create"),
+    path('<int:pk>/update', views.update, name="update"),
+    path('<int:pk>/delete', views.delete, name="delete"),
 ]
 
 #현재폴더는 .
